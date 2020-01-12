214b888bb668380de11fba188c9e9493aee0751a
abathingnerd <59466021+abathingnerd@users.noreply.github.com>
2020-01-11 18:35:15 -0800

    added-get-request-from-t7pi

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const app = document.getElementById('root');
+console.log(app);
 
 const logo = document.createElement('img');
 logo.src = 'logo.png';
@@ -8,3 +9,26 @@ container.setAttribute('class', 'container');
 
 app.appendChild(logo);
 app.appendChild(container);
+
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://cors-anywhere.herokuapp.com/http://t7api.herokuapp.com/character/list', true)
+
+request.onload = function() {
+    //Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+
+    data.forEach(character => {
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+
+        const h1 = document.createElement('h1');
+        h1.textContent = ' '+character.label;
+
+        container.appendChild(card);
+        card.appendChild(h1);
+    });
+}
+
+//Send Request
+request.send()
