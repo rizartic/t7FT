32b03a869587be1ff8178f75a4dd7f41378f4de0
abathingnerd <59466021+abathingnerd@users.noreply.github.com>
2020-01-19 12:03:37 -0800

    Search a character's movelist for a move, return a list of moves matching that input

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var dict = {
     "jin" : "jin",
     "kaz" : "kazuya",
     "kazuya" : "kazuya",
+    "josie" : "josie",
 
 }
 var input = document.getElementById("charName");
@@ -15,20 +16,19 @@ function(event) {
 });
 
 function trySearch() {
-    var name = document.getElementById("charName").value;
-    if(dict[name]) {
+    var chname = document.getElementById("charName").value;
+    var strname = document.getElementById("moveName").value;
+    if(dict[chname]) {
        alert("We're in");
+       performSearch(chname, strname);
     }
     else {
         alert("No good");
     }
 
-    document.getElementById("demo").innerHTML = name;
 }
-
 const app = document.getElementById('root');
 console.log(app);
-
 const logo = document.createElement('img');
 logo.src = 'logo.png';
 
@@ -38,35 +38,38 @@ container.setAttribute('class', 'container');
 app.appendChild(logo);
 app.appendChild(container);
 
-
-var request = new XMLHttpRequest()
-//hey this is a test
-request.open('GET', 'https://cors-anywhere.herokuapp.com/http://t7api.herokuapp.com/character/josie?cmd=df2', true)
-
-
-//Send Request
-request.send()
+function performSearch(chname, strname) {
+    var request = new XMLHttpRequest()
+    //hey this is a test
+    var userInput = 'https://cors-anywhere.herokuapp.com/http://t7api.herokuapp.com/character/' + chname + '?cmd=' + strname;
+    // request.open('GET', 'https://cors-anywhere.herokuapp.com/http://t7api.herokuapp.com/character/josie?cmd=df2', true)
+    request.open('GET', userInput, true);
 
 
-// **for testing individual console gets
-request.onload = function() {
-    //** note in JS, vars can be arrays.
-    // In this example, fuzzy searching a command will
-    // will return an array of all strings containing that command*/
-    
-    
-    var data = JSON.parse(this.response) //think its necessary for using data? convert json to object / array of objects
-    // console.log(JSON.stringify(data))
-    // console.log(data.length) //return length of array
-    // console.log(data[0].speed) //return the speed of the first move in array
-    
-    // data.forEach(function(element) { //return all strings the search found
-    //     console.log(element.cmd)
+    //Send Request
+    request.send()
 
-    // })
-    // console.log(data)
+
+    // **for testing individual console gets
+    request.onload = function() {
+            //** note in JS, vars can be arrays.
+            // In this example, fuzzy searching a command will
+            // will return an array of all strings containing that command*/
+            
+            
+            var data = JSON.parse(this.response) //think its necessary for using data? convert json to object / array of objects
+            console.log(data);
+            // console.log(JSON.stringify(data))
+            // console.log(data.length) //return length of array
+            // console.log(data[0].speed) //return the speed of the first move in array
+            
+            // data.forEach(function(element) { //return all strings the search found
+            //     console.log(element.cmd)
+
+            // })
+            // console.log(data)
+        }
 }
-
 // // 
 // request.onload = function() {
 //     //Begin accessing JSON data here
