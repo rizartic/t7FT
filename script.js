930447d99a8807b29875de0bde639df343493fc6
rizartic <articulo@unlv.nevada.edu>
2020-01-28 19:05:31 -0800

    Figured out how async functions work
    Function now searches for a character's move, and if it can generate a frame trap, will continue to search the rest of the character's movelist. Must now implement finding frametrap functionality.

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ function trySearch() {
     }
 
 }
+
+/* Creating some elements on the webpage, for the looks */
 const app = document.getElementById('root');
 console.log(app);
 const logo = document.createElement('img');
@@ -42,45 +44,37 @@ function performSearch(chname, strname) {
     return new Promise(resolve => {
         var data;
         var request = new XMLHttpRequest();
-        //hey this is a test
-        var userInput = 'https://cors-anywhere.herokuapp.com/http://t7api.herokuapp.com/character/' + dict[chname] + '?cmd=' + strname;
-        // request.open('GET', 'https://cors-anywhere.herokuapp.com/http://t7api.herokuapp.com/character/josie?cmd=df2', true)
-        request.open('GET', userInput, true);
+        var fetchUrl = 'https://cors-anywhere.herokuapp.com/http://t7api.herokuapp.com/character/' + dict[chname];
+        if (strname) { //if there was something included in the string field
+             fetchUrl = fetchUrl + '?cmd=' + strname;
+        }
+        request.open('GET', fetchUrl, true);
         // **for testing individual console gets
+        // request.open('GET', 'https://cors-anywhere.herokuapp.com/http://t7api.herokuapp.com/character/josie', true)
         request.onload = function () {
             //** note in JS, vars can be arrays.
             // In this example, fuzzy searching a command will
             // will return an array of all strings containing that command*/
             data = JSON.parse(this.response); //think its necessary for using data? convert json to object / array of objects
-            // console.log(data);
+            resolve(data);
         }
-
         //Send Request
         request.send();
-        // console.log(data);
-        console.log('in performSearch...');
     });
 }
 
-
-// return new Promise(resolve => {
-//     setTimeout(function () {
-//         resolve("slow")
-//         console.log("slow promise is done")
-//     }, 2000)
-// })
 async function findFrameTrap(chname, strname) {
     console.log('Now in findFrameTrap...');
-    var data = await performSearch(chname, strname);
+    var searchResult = await performSearch(chname, strname);
 
-    console.log(data);
+    console.log(searchResult);
     console.log('And we are back...');
-    if (data.length == 0) {
+    if (searchResult.length == 0) {
         console.log("No commands found here");
     }
     else { //here, we'll look for frame traps using the move given.
         // console.log(data);
-        var firstMove = data[0];
+        var firstMove = searchResult[0];
         console.log('-------------------------');
         console.log('Command: ' + firstMove.cmd);
         console.log('OB: ' + firstMove.onBlock);
@@ -101,37 +95,11 @@ async function findFrameTrap(chname, strname) {
         /* Let's first check for TRUE frame traps,
         meaning the second move is a mid. */
         else {
-
+            searchResult = await performSearch(chname, 0); //dont want a specific string; want the entire array here
+            console.log(searchResult);
         }
 
 
     }
 
 }
-
-        // console.log(JSON.stringify(data))
-        // console.log(data.length) //return length of array
-        // console.log(data[0].speed) //return the speed of the first move in array
-
-        // data.forEach(function(element) { //return all strings the search found
-        //     console.log(element.cmd)
-
-        // })
-        // console.log(data)
-
-// //
-// request.onload = function() {
-//     //Begin accessing JSON data here
-//     var data = JSON.parse(this.response)
-
-//     data.forEach(character => {
-//         const card = document.createElement('div');
-//         card.setAttribute('class', 'card');
-
-//         const h1 = document.createElement('h1');
-//         h1.textContent = ' '+character.label;
-
-//         container.appendChild(card);
-//         card.appendChild(h1);
-//     });
-// }
