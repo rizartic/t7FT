5e4ee80e1f9994b1265b862d615e14b136a562ad
rizartic <articulo@unlv.nevada.edu>
2020-02-18 21:23:37 -0800

    Printing recommended moves to table

--- table.js
+++ table.js
@@ -8,9 +8,25 @@ function build_table(rcmdArr) {
     var fdtable = document.getElementById('fdTable');
     var tableSize = fdtable.rows.length;
     console.log("Table rows: " + fdtable.rows.length);
+    console.log("rmcdArr size: " + rcmdArr.length);
     if (tableSize != 1) {
-        reset_table(fdTable);
+        reset_table(fdtable);
     }
+    console.log(rcmdArr[0]);
+    console.log(typeof (rcmdArr));
+    rcmdArr.forEach(move => {
+
+            var newRow = fdtable.insertRow(fdtable.rows.length);
+            var cmdcell = newRow.insertCell(0);
+            var hitcell = newRow.insertCell(1);
+            var spdcell = newRow.insertCell(2);
+            var dmgcell = newRow.insertCell(3);
+
+            cmdcell.innerHTML = move.cmd;
+            hitcell.innerHTML = move.hit;
+            spdcell.innerHTML = move.speed;
+            dmgcell.innerHTML = move.dmg;
+        });
 }
 
 
