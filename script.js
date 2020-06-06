75a38c7963ca354e978c85d5fd4e0eb32d011bec
abathingnerd <59466021+abathingnerd@users.noreply.github.com>
2020-06-06 15:02:43 -0700

    design-practice sturf

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Event listener when clicking on character card.
-document.querySelector('.card').addEventListener("click", function(){
-    window.alert('Hit');
-});
+// document.querySelector('.card').addEventListener("click", function(){
+//     window.alert('Hit');
+// });
 
 //Event listener when closing the character modal.
 // document.querySelector('.close').addEventListener('click', function(){
 //     document.querySelector('.character-info-window-overlay').style.display='none';
 // });
 
-// const app = document.getElementById('root');
+const app = document.getElementById('character-select');
 // console.log(app);
 
 // const container = document.createElement('div');
@@ -23,21 +23,31 @@ var proxy = 'https://cors-anywhere.herokuapp.com/'
 request.open('GET', proxy+'http://t7api.herokuapp.com/character/list', true)
 
 request.onload = function() {
-    //Begin accessing JSON data here
     var data = JSON.parse(this.response)
-
-    data.forEach(character => {
+    let charactersUnsorted = data.map (character => character.name)
+    characters = charactersUnsorted.sort();
+ 
+    characters.forEach(name => {
         
+        //Create div element for card
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
+        card.setAttribute('id', 'card-'+name);
 
-        const h1 = document.createElement('h1');
-        h1.textContent = ' '+character.label;
+        //Create image element for card
+        const thumbnail = document.createElement('img');
+        thumbnail.setAttribute('src', 'images/thumbnails/'+name+'-thumbnail.jpg');
 
-        container.appendChild(card);
-        card.appendChild(h1);
+        //create div element for name
+        const characterName = document.createElement('div');
+        characterName.setAttribute('class', 'card-name');
+        characterName.setAttribute('id', 'card-'+name);
+        characterName.textContent = (name);
 
-        
+        app.appendChild(card);
+        card.appendChild(thumbnail);
+        card.appendChild(characterName);
+        console.log(name);
     });
 }
 
